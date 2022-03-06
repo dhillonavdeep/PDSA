a8847a4d73a9568a32e8db727ac7d37f86b80a4b
Navdeep <navdeepdhillon2020@gmail.com>
2022-03-06 20:44:39 +0530

    1st code

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,1 +1,16 @@
-print("hek")
+def selection_sort(l):
+    n=len(l)
+    i=0
+    if n<1:
+        return(l)   
+    for i in range(n):
+        mpos=i
+        for j in range(i,n):
+            if l[j]<l[mpos]:
+                mpos=j
+        (l[i],l[mpos])=(l[mpos],l[i])
+    return(l)
+
+x=[3,1,2]
+selection_sort(x)
+print(x)
